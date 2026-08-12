d20f16dcc4df20688c41633ff2980847ed248169
onadith-thecoder <onadhith@gmail.com>
2026-08-12 19:53:05 +0530

    feat: Add function for list the top ten memory-consuming processes including PID, User, CPU, and memory

--- task1_iot_monitor.sh
+++ task1_iot_monitor.sh
@@ -12,11 +12,20 @@ log_action() {
 }
 
 ## (a) Process monitoring and management ## 
-#function - Display CPU and memory usage
-show_cpu_mem() {
-    echo "**** CPU & Memory Usage ****"
-    echo "CPU Load (1/10/20/30 min): $(uptime | awk -F'load average:' '{print $2}')"
-    echo "Memory Usage:"
-    free -h
-    log_action "Displayed CPU and memory usage"
-}
+    #function - Display CPU and memory usage
+    show_cpu_mem() {
+        echo "**** CPU & Memory Usage ****"
+        echo "CPU Load (1/10/20/30 min): $(uptime | awk -F'load average:' '{print $2}')"
+        echo "Memory Usage:"
+        free -h
+        log_action "Displayed CPU and memory usage"
+    }
+
+    #function - Listing  the top ten memory-consuming processes including PID, user, CPU percentage and memory percentage.
+    list_top_procs() {
+        echo "**** Top 10 Memory-consuming Processes ****"
+        echo "PID   USER        %CPU    %MEM    %COMMAND"
+        ps aux --sort=-%mem | head -11 | awk 'NR>1 {printf "%-6s %-10s %-6s %-6s %s\n", $2, $1, $3, $4, $11}'
+        log_action "Listed top 10 memory processes"
+    }
+

92a96e35bc656d03b2d6d677fcd85e050e1a7005
onadith-thecoder <onadhith@gmail.com>
2026-08-12 19:28:17 +0530

    feat: Add function for display CPU and memory usage

--- task1_iot_monitor.sh
+++ task1_iot_monitor.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 # Smart Campus IoT Device Management Script
 
+#Maintain a system_monitor_log.txt file.
 LOG_FILE="system_monitor_log.txt"
 ARCHIVE_DIR="ArchiveLogs"
 CRITICAL_NAMES=("systemd" "init" "sshd" "cron" "kernel" "kthreadd")
 
-#log action - function
+#log action function - Record all administrative actions with timestamps
 log_action() {
     echo "$(date '+%Y-%m-d% %H:%M:%S') - $1" >> "$LOG_FILE"
+}
+
+## (a) Process monitoring and management ## 
+#function - Display CPU and memory usage
+show_cpu_mem() {
+    echo "**** CPU & Memory Usage ****"
+    echo "CPU Load (1/10/20/30 min): $(uptime | awk -F'load average:' '{print $2}')"
+    echo "Memory Usage:"
+    free -h
+    log_action "Displayed CPU and memory usage"
 }

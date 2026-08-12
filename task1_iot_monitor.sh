90fdfb984693f5cd6430115a8c4818b4469ae000
onadith-thecoder <onadhith@gmail.com>
2026-08-12 18:17:33 +0530

    assign a specific text file to record logs, assign the log file to store old logs, updated command to protect important system programs

---
+++ task1_iot_monitor.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Smart Campus IoT Device Management Script
+
+LOG_FILE="system_monitor_log.txt"
+ARCHIVE_DIR="ArchiveLogs"
+CRITICAL_NAMES=("systemd" "init" "sshd" "cron" "kernel" "kthreadd")
+
+

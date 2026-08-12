996eb14743d8d8513838723d011449fc91de550c
onadith-thecoder <onadhith@gmail.com>
2026-08-12 19:03:01 +0530

    feat: Add function for record all administrative actions with timestamps

--- task1_iot_monitor.sh
+++ task1_iot_monitor.sh
@@ -5,4 +5,7 @@ LOG_FILE="system_monitor_log.txt"
 ARCHIVE_DIR="ArchiveLogs"
 CRITICAL_NAMES=("systemd" "init" "sshd" "cron" "kernel" "kthreadd")
 
-
+#log action - function
+log_action() {
+    echo "$(date '+%Y-%m-d% %H:%M:%S') - $1" >> "$LOG_FILE"
+}

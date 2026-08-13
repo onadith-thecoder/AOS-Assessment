419af2e5b9b065c0570595b8133eeaae00d4e735
onadith-thecoder <onadhith@gmail.com>
2026-08-14 00:08:29 +0530

    feat: Added a function for job submit and integrated write_log for keep log info of the job.

--- task2_job_scheduler.sh
+++ task2_job_scheduler.sh
@@ -12,7 +12,7 @@ write_log() {
     echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" >> "$LOG_FILE"
 }
 
-#Show all jobs waiting in line
+#function: Show all jobs waiting in line
 show_Waiting_jobs() {
     if [[ ! -f "$QUEUE_FILE" || ! -s "$QUEUE_FILE" ]]; then
         echo "No waiting jobs. The line is empty!"
@@ -20,5 +20,21 @@ show_Waiting_jobs() {
         echo "Waiting Jobs in line (Format: StudentsID|JobName|ExecTime|Priority):"
         cat "$QUEUE_FILE"
     fi
+}
+
+#function: Add a new job
+add_job() {
+    read -p "Student ID: " sid
+    read -p "Job Description: " jdesc
+    read -p "How many secounds will it takes? " etime
+    read -p "Priority (1 most, 10 least): " prior
+
+    if [[ ! "$etime" =~ ^[0-9]+$ ]] || [[ ! "$prior" =~ ^[1-9]$|^10$ ]]; then
+        echo "Invalid time or priority."
+        return
+    fi
+        echo "$sid|$jdesc|$etime|$prior" >> "$QUEUE_FILE"
+        write_log "job added: $sid|$jdesc|$etime|$prior"
+        echo "Job added!"
 }
 
